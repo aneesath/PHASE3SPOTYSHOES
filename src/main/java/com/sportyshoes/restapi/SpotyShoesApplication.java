fbf22ff1acc70d078de25d2d4a9c5eb570a024b8
aneesath <aneesathkp@gmail.com>
2023-08-18 00:12:58 +0530

    first commit

---
+++ src/main/java/com/sportyshoes/restapi/SpotyShoesApplication.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotyShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotyShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportyshoes/restapi/SpotyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
